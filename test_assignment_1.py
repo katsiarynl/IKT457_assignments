a73073b17c49e7befe381be57ef392201f141f82
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2024-09-04 16:50:11 +0200

    :sparkles: add histogram to assignment 1

--- test_assignment_1.py
+++ test_assignment_1.py
@@ -1,58 +1,44 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
+
 class Environment:
     def __init__(self, penalty_probability):
-        self.penalty_probability = penalty_probability #probability of receiving a punishment regardless of the action
-         
+        self.penalty_probability = penalty_probability
 
     def penalty(self, counter, ta):
-        random_value=random.random()
-        if counter>=0 and counter<4: 
-           
-            self.penalty_probability=1-(counter*0.2)
-
-            print("Random number is", ta, "is", random_value, "\n")
-            print("Penalty prob for", ta, "is", self.penalty_probability, "\n")
+        random_value = random.random()
+        if counter >= 0 and counter < 4:
+            self.penalty_probability = 1 - (counter * 0.2)
             if random_value <= self.penalty_probability:
-                    return True
+                return True
             else:
-                    return False
-        
-        if counter==4 or counter==5:
-            self.penalty_probability=1-(0.6-(counter-3)*0.2)
-            print("Penalty prob for", ta, "is", self.penalty_probability, "\n")
-            if random_value <= self.penalty_probability:
-                    return True
-            else:
-                    return False
-     
-            
+                return False
 
-  
+        if counter == 4 or counter == 5:
+            self.penalty_probability = 1 - (0.6 - (counter - 3) * 0.2)
+            if random_value <= self.penalty_probability:
+                return True
+            else:
+                return False
 
 class Tsetlin:
     def __init__(self, n):
-        # n is the number of states per action
         self.n = n
+        self.state = random.choice([self.n, self.n + 1])
 
-        # Initial state selected randomly
-        self.state = random.choice([self.n, self.n+1])
-
-    #no changes here
     def reward(self):
         if self.state <= self.n and self.state > 1:
             self.state -= 1
-        elif self.state > self.n and self.state < 2*self.n:
+        elif self.state > self.n and self.state < 2 * self.n:
             self.state += 1
-
 
     def penalize(self):
         if self.state <= self.n:
             self.state += 1
         elif self.state > self.n:
             self.state -= 1
-    #a little change here:
 
     def makeDecision(self):
         if self.state <= self.n:
@@ -60,101 +46,88 @@ class Tsetlin:
         else:
             return "no"
 
-
-yes_no_count=[0,0]
-
 env = Environment(1)
 
 la1 = Tsetlin(3)
 la2 = Tsetlin(3)
 la3 = Tsetlin(3)
-la4= Tsetlin(3)
-la5= Tsetlin(3)
-array=[]
+la4 = Tsetlin(3)
+la5 = Tsetlin(3)
+array = []
 
-for i in range(100):
-    word1=la1.makeDecision()
-    word2=la2.makeDecision()
-    word3=la3.makeDecision()
-    word4=la4.makeDecision()
-    word5=la5.makeDecision()
+fig, ax = plt.subplots()
+bins = np.arange(0, 7) - 0.5  # To center the bins around integers
+ax.set_xlim(-0.5, 5.5)
+ax.set_ylim(0, 100)  # Adjust the y-axis limit as needed
+ax.set_xlabel("Number of TA saying yes")
+ax.set_ylabel("Frequency")
+ax.set_title("Histogram of Yes Counts Over Iterations")
+bars = ax.hist([], bins=bins, edgecolor="black")[2]
 
-    yes_and_no=[word1, word2, word3, word4, word5]
-    print("=========Loop ",i,"==========\n")
-    print(yes_and_no)
- 
-    counter=yes_and_no.count("yes")
+def update_hist(num):
+    word1 = la1.makeDecision()
+    word2 = la2.makeDecision()
+    word3 = la3.makeDecision()
+    word4 = la4.makeDecision()
+    word5 = la5.makeDecision()
+
+    yes_and_no = [word1, word2, word3, word4, word5]
+    counter = yes_and_no.count("yes")
     array.append(counter)
-    print("Counter: ", counter, "\n")
-    print("^^^^^^^^^^^^^^^^^^^\n")
+
     penalty_for_la1 = env.penalty(counter, "LA1")
     penalty_for_la2 = env.penalty(counter, "LA2")
     penalty_for_la3 = env.penalty(counter, "LA3")
-    penalty_for_la4 = env.penalty(counter, "LA4") 
+    penalty_for_la4 = env.penalty(counter, "LA4")
     penalty_for_la5 = env.penalty(counter, "LA5")
-    print("^^^^^^^^^^^^^^^^^^^\n")
-  
-         
+
     if penalty_for_la1:
-        print("Penalty for LA1", end = ' ')
         la1.penalize()
     else:
-        print("Reward for LA1", end = ' ')
         la1.reward()
-  
 
     if penalty_for_la2:
-        print("Penalty for LA2", penalty_for_la2, end = ' ')
         la2.penalize()
     else:
-        print("Reward for LA2", end = ' ')
         la2.reward()
 
     if penalty_for_la3:
-        print("Penalty for LA3", end = ' ')
         la3.penalize()
     else:
-        print("Penalty for LA3", end = ' ')
         la3.reward()
 
-
     if penalty_for_la4:
-        print("Penalty for LA4", end = ' ')
         la4.penalize()
     else:
-        print("Reward for LA4", end = ' ')
         la4.reward()
 
     if penalty_for_la5:
-        print("Penalty for LA5", end = ' ')
         la5.penalize()
     else:
-        print("Reward for LA5", end = ' ')
         la5.reward()
-    
-    print("\n")
-    print("===================\n")
 
+    ax.cla()  # Clear the previous histogram
+    ax.hist(array, bins=bins, edgecolor="black")
+    ax.set_xlim(-0.5, 5.5)
+    ax.set_ylim(0, 100)
+    ax.set_xlabel("Number of TA saying yes")
+    ax.set_ylabel("Frequency")
+    ax.set_title("Histogram")
 
+number_of_frames=101
+ani = FuncAnimation(fig, update_hist, frames=number_of_frames-1, repeat=False, interval=200)
 
+plt.show()
 
-
-plt.rcParams["figure.figsize"] = [7.50, 3.50]
-plt.rcParams["figure.autolayout"] = True
 
 y = array
-x = range(100)
+x = range(number_of_frames)
 
-plt.title("Line graph")
+plt.title("Graph")
 plt.plot(x, y, color="red")
 
 plt.xlabel("Loop")
 plt.ylabel("Number of TA saying yes")
 plt.show()
 
-# M=0 penalty=1
-#M 1  penalty 0.8
-# M 2 penlty = 0.6
-# M3 penalty 0.4
-# M4 penalty 0.6
-# M5 penalty 0.8
+
